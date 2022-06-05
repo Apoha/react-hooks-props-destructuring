5b506f35548ea14ed0b37d8b36825878f5d772d4
Apoh <Edenra08@gmail.com>
2022-06-05 06:36:13 +0100

    Assignment viewed

---
+++ src/components/SocialMedia.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function SocialMedia({ socialLinks }) {
+    return (
+      <div>
+        <a href={socialLinks.github}>{socialLinks.github}</a>
+        <a href={socialLinks.linkedin}>{socialLinks.linkedin}</a>
+      </div>
+    );
+  }
+
+
+  export default SocialMedia;
